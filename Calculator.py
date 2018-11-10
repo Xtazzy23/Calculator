0e64aad1dcb4c85ee6a5008a99334835e8940b67
Xtazzy23 <44172780+Xtazzy23@users.noreply.github.com>
2018-11-10 10:00:51 +0530

    Initial commit

---
+++ Calculator.py
@@ -0,0 +1,8 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+print(num1 + num2)
+# Variable numbers for input, output displays first number followed by second number
+result = float(num1) + float(num2)
+print(result)
+# Operator integers whole values to display result
+
